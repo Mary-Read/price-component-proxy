99e2fa28b8b0716338e948f641e8184bdf068c7a
Darian <66260540+DarianAltF4@users.noreply.github.com>
2021-05-27 23:56:11 -0400

    updated proxy. All components Rendered

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
-const port = 6000;
+
+const port = 5000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/../Client/')))
+app.use(express.static(path.join(__dirname, '/../Client/')));
+app.use(cors())
 
 app.get(['/', '/p/*'], (req, res) => {
+  console.log('Made It')
   res.sendFile(path.join(__dirname, '/../Client/index.html'));
 });
+
+app.get('/test', (req, res) =>{
+  console.log('Test')
+})
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
